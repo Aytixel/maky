b2d0c7488db60fd0da434ab71caeb629da61d952
Aytixel <aytixel@gmail.com>
2024-03-02 18:48:09 +0100

    Clean command working with dependencies

--- src/command/clean.rs
+++ src/command/clean.rs
@@ -1,4 +1,9 @@
-use std::{fs::remove_dir_all, io};
+use std::{
+    env,
+    fs::remove_dir_all,
+    io,
+    process::{Command, Stdio},
+};
 
 use crate::config::{LoadConfig, ProjectConfig};
 
@@ -9,34 +14,52 @@ pub fn clean(config_file: String) -> io::Result<()> {
 
     match ProjectConfig::load(project_config_path) {
         Ok(project_config) => {
-            remove_dir_all(project_path.join(project_config.binaries))?;
-            remove_dir_all(project_path.join(project_config.objects))?;
+            remove_dir_all(project_path.join(".maky/include")).ok();
+            remove_dir_all(project_path.join(project_config.binaries)).ok();
+            remove_dir_all(project_path.join(project_config.objects)).ok();
 
             for os_specific_config in project_config.os_specific.values() {
                 if let Some(binaries) = &os_specific_config.binaries {
-                    remove_dir_all(project_path.join(binaries))?;
+                    remove_dir_all(project_path.join(binaries)).ok();
                 }
                 if let Some(objects) = &os_specific_config.objects {
-                    remove_dir_all(project_path.join(objects))?;
+                    remove_dir_all(project_path.join(objects)).ok();
                 }
             }
 
             for arch_specific_config in project_config.arch_specific.values() {
                 if let Some(binaries) = &arch_specific_config.binaries {
-                    remove_dir_all(project_path.join(binaries))?;
+                    remove_dir_all(project_path.join(binaries)).ok();
                 }
                 if let Some(objects) = &arch_specific_config.objects {
-                    remove_dir_all(project_path.join(objects))?;
+                    remove_dir_all(project_path.join(objects)).ok();
                 }
             }
 
             for feature_specific_config in project_config.feature_specific.values() {
                 if let Some(binaries) = &feature_specific_config.binaries {
-                    remove_dir_all(project_path.join(binaries))?;
+                    remove_dir_all(project_path.join(binaries)).ok();
                 }
                 if let Some(objects) = &feature_specific_config.objects {
-                    remove_dir_all(project_path.join(objects))?;
+                    remove_dir_all(project_path.join(objects)).ok();
                 }
+            }
+
+            /*
+                clean dependencies
+            */
+
+            for dependency_path in project_config.dependencies.values() {
+                let dependency_path = project_path.join(dependency_path);
+                let mut command = Command::new(env::current_exe().unwrap());
+
+                command
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::inherit())
+                    .arg("clean")
+                    .arg("-f")
+                    .arg(&dependency_path)
+                    .status()?;
             }
 
             Ok(())
